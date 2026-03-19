82789b12e7b004f280ed71dfdbbcb6bb3d2b208a
julialta17-del <julialta17@gmail.com>
2026-03-19 09:08:30 -0300

    Update fudo_bot_prueba_noche.py

--- fudo_bot_prueba_noche.py
+++ fudo_bot_prueba_noche.py
@@ -48,7 +48,7 @@ def ejecutar_todo():
         driver.get("https://app-v2.fu.do/app/#!/sales")
         
         # Login
-        wait.until(EC.presence_of_element_located((By.ID, "user"))).send_keys("gestion@bigsaladsquinta")
+        wait.until(EC.presence_of_element_located((By.ID, "user"))).send_keys("FUDO_USER")
         driver.find_element(By.ID, "password").send_keys("BigQuinta22")
         driver.find_element(By.ID, "password").submit()
         
