d26709e3b9fcbeecf7b352ade44053eb42a701c3
julialta17-del <julialta17@gmail.com>
2026-03-23 22:06:54 -0300

    Update analisis_noche.py

--- analisis_noche.py
+++ analisis_noche.py
@@ -76,7 +76,7 @@ try:
     
     # Login
     wait.until(EC.presence_of_element_located((By.ID, "user"))).send_keys("gestion@bigsaladsquinta")
-    driver.find_element(By.ID, "password").send_keys("BigQuinta22")
+    driver.find_element(By.ID, "password").send_keys("FUDO_PASS")
     driver.find_element(By.ID, "password").submit()
 
     print(f"Esperando carga y aplicando rango: {fecha_inicio} a {fecha_fin}")
